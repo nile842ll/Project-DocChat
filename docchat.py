93a41580350d2eccd399883a7ea7ff16fc66f816
lauren <llambrecht58@cmc.edu>
2025-04-29 00:02:44 -0700

    Update docchat.py

--- docchat.py
+++ docchat.py
@@ -128,24 +128,8 @@ def add(a, b):
     """
     return a + b
 
-if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
-
-
-main()
-
-def add(a, b):
-    """
-    Adds two numbers together.
-
-    >>> add(2, 3)
-    5
-    >>> add(-1, 1)
-    0
-    """
-    return a + b
 
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
+    main()
