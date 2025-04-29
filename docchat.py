8d161987d2ab832b56e17c832fc823214fdf6443
lauren <llambrecht58@cmc.edu>
2025-04-29 00:43:26 -0700

    Update docchat.py

--- docchat.py
+++ docchat.py
@@ -65,12 +65,15 @@ def load_text(filepath_or_url):
 
 
 def chunk_text_by_words(text, max_words=100, overlap=50):
-    """
+    '''
     Splits text into overlapping chunks of words.
 
-    >>> chunk_text_by_words("one two three four five six", 4, 2)
-    ['one two three four', 'three four five six']
-    """
+    >>> chunks = chunk_text_by_words("one two three four five six", 4, 2)
+    >>> len(chunks)
+    2
+    >>> "three four" in chunks[1]
+    True
+    '''
     words = text.split()
     chunks = []
     for i in range(0,len(words), max_words-overlap):
